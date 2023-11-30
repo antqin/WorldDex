abac8828ad1e007ba60c876bb7e8fd1093b7399e
Jaiden Reddy <jaidenreddy@gmail.com>
2023-11-29 17:13:57 -0800

    reworked server in python

---
+++ WorldHost/parse_public.py
@@ -0,0 +1,210 @@
+from hexbytes import HexBytes
+from coincurve import PublicKey as CCPublicKey
+from eth_account._utils.signing import to_standard_v
+from eth_account._utils.legacy_transactions import serializable_unsigned_transaction_from_dict
+from eth_keys.datatypes import Signature
+from eth_rlp import HashableRLP
+from web3 import Web3
+import requests
+import base64
+from base64 import b64encode
+from ecies import encrypt, decrypt
+from ecies.utils import generate_eth_key
+from cryptography.fernet import Fernet
+import os
+
+def encrypt_image(image_data_bytes, eth_public_key_hex, output_encrypted_image_path='encrypted_image.enc'):
+    """
+    Encrypts an image using a symmetric key and then encrypts the key using an Ethereum public key.
+
+    Args:
+        image_data_bytes (bytes): Image data in bytes.
+        eth_public_key_hex (str): Ethereum public key in hex.
+        output_encrypted_image_path (str): Path to save the encrypted image.
+
+    Returns:
+        str: Hexadecimal representation of the encrypted symmetric key.
+    """
+    # Generate a symmetric key for Fernet encryption
+    symmetric_key = Fernet.generate_key()
+    cipher_suite = Fernet(symmetric_key)
+
+    # Encrypt the image
+    encrypted_image = cipher_suite.encrypt(image_data_bytes)
+
+    # Save the encrypted image to a file
+    with open(output_encrypted_image_path, 'wb') as encrypted_image_file:
+        encrypted_image_file.write(encrypted_image)
+
+    # Encrypt the symmetric key using the Ethereum public key
+    encrypted_symmetric_key = encrypt(eth_public_key_hex, symmetric_key)
+
+    return encrypted_symmetric_key.hex()
+
+
+def get_most_recent_txid(eth_address, api_key, chain):
+    """Fetches the most recent transaction ID for a given Ethereum address.
+
+    Args:
+        eth_address (str): The Ethereum address.
+        api_key (str): The API key for Etherscan.
+
+    Returns:
+        str: The transaction ID of the most recent transaction.
+    """
+    if chain == "SEP":
+      etherscan_api_url = "https://api-sepolia.etherscan.io/api"
+    else:
+      etherscan_api_url = "https://api.etherscan.io/api"
+    params = {
+        'module': 'account',
+        'action': 'txlist',
+        'address': eth_address,
+        'startblock': 0,
+        'endblock': 99999999,
+        'page': 1,
+        'offset': 1,
+        'sort': 'desc',
+        'apikey': api_key
+    }
+
+    response = requests.get(etherscan_api_url, params=params)
+    data = response.json()
+
+    if data['status'] == '1' and data['message'] == 'OK' and len(data['result']) > 0:
+        return data['result'][0]['hash']
+    else:
+        raise Exception("No transactions found for this address or error in API call.")
+
+def pub_key_from_tx_eth(txid, chain, web3provider):
+    w3test = Web3(Web3.HTTPProvider(web3provider))
+    transaction = w3test.eth.get_transaction(txid)
+    chain_id = "0xAA36A7" if chain == "SEP" else "0x01"  # Use integer value for chain ID
+
+    # Determine if it's an EIP-1559 transaction
+    if 'maxFeePerGas' in transaction and 'maxPriorityFeePerGas' in transaction:
+        tx_dict = {
+            'nonce': transaction['nonce'],
+            'maxPriorityFeePerGas': transaction['maxPriorityFeePerGas'],
+            'maxFeePerGas': transaction['maxFeePerGas'],
+            'gas': transaction['gas'],
+            'to': transaction['to'],
+            'value': transaction['value'],
+            'data': transaction.get('input', ''),
+            'chainId': chain_id,
+            'type': transaction['type']  # Ensure to include the type field for EIP-1559 transactions
+        }
+    else:
+        # Handle as a legacy transaction
+        tx_dict = {
+            'nonce': transaction['nonce'],
+            'gasPrice': transaction['gasPrice'],
+            'gas': transaction['gas'],
+            'to': transaction['to'],
+            'value': transaction['value'],
+            'data': transaction.get('input', ''),
+            'chainId': chain_id
+        }
+
+    # Create a serializable unsigned transaction
+    serialized_tx = serializable_unsigned_transaction_from_dict(tx_dict)
+
+    # Recover the public key
+    vrs = (to_standard_v(transaction['v']),
+           int.from_bytes(transaction['r'], "big"),
+           int.from_bytes(transaction['s'], "big"))
+    signature = Signature(vrs=vrs)
+    rec_pub = signature.recover_public_key_from_msg_hash(serialized_tx.hash())
+    
+
+    # Verify the recovered address
+    if rec_pub.to_checksum_address() != transaction['from']:
+        raise ValueError("Unable to obtain public key from transaction: " + txid)
+
+    return rec_pub, rec_pub.to_checksum_address()
+  
+def eth_address_to_pub_key(eth_address, api_key, chain, web3provider):
+    """
+    Converts a Sepolia Ethereum address into a public key by finding the most recent transaction ID
+    and then using it to obtain the public key.
+
+    Args:
+        eth_address (str): The Ethereum address.
+        api_key (str): The API key for Etherscan.
+        chain (str): The blockchain network (e.g., 'SEP' for Sepolia).
+
+    Returns:
+        tuple: A tuple containing the public key and the corresponding Ethereum address.
+    """
+    try:
+        # Get the most recent transaction ID for the given Ethereum address
+        txid = get_most_recent_txid(eth_address, api_key, chain)
+
+        # Obtain the public key from the Ethereum transaction
+        public_key, derived_address = pub_key_from_tx_eth(txid, chain, web3provider)
+
+        return public_key, derived_address
+    except Exception as e:
+        raise Exception(f"Error in processing: {e}")
+      
+def encrypt_image_with_eth_address(eth_address, api_key, chain, image_data_bytes, web3provider, output_encrypted_image_path='encrypted_image.enc'):
+    """
+    Encrypts an image using a public key derived from an Ethereum address.
+
+    Args:
+        eth_address (str): The Ethereum address.
+        api_key (str): The API key for Etherscan.
+        chain (str): The blockchain network (e.g., 'SEP' for Sepolia).
+        image_path (str): Path to the image to be encrypted.
+        output_encrypted_image_path (str): Path to save the encrypted image.
+
+    Returns:
+        str: Hexadecimal representation of the encrypted symmetric key.
+    """
+    try:
+        # Get the public key from the Ethereum address
+        public_key, _ = eth_address_to_pub_key(eth_address, api_key, chain, web3provider)
+        public_key_hex = public_key.to_hex()
+
+        # Encrypt the image using the retrieved public key
+        encrypted_symmetric_key_hex = encrypt_image(image_data_bytes, public_key_hex, output_encrypted_image_path)
+
+        print("Encrypted symmetric key:", encrypted_symmetric_key_hex)
+        return encrypted_symmetric_key_hex
+    except Exception as e:
+        raise Exception(f"Error in processing: {e}")
+      
+def decrypt_image(eth_private_key_hex, encrypted_symmetric_key_b64, encrypted_image_path, output_decrypted_image_path='decrypted_image.png'):
+    # Base64 Decode the encrypted symmetric key
+    encrypted_symmetric_key = base64.b64decode(encrypted_symmetric_key_b64)
+
+    # Decrypt the symmetric key with the Ethereum private key
+    decrypted_symmetric_key = decrypt(eth_private_key_hex, encrypted_symmetric_key)
+
+    # Read the encrypted image
+    with open(encrypted_image_path, 'rb') as encrypted_image_file:
+        encrypted_image_data = encrypted_image_file.read()
+
+    # Decrypt the image with the symmetric key
+    cipher_suite = Fernet(decrypted_symmetric_key)
+    decrypted_image_data = cipher_suite.decrypt(encrypted_image_data)
+
+    # Save the decrypted image back to a file
+    with open(output_decrypted_image_path, 'wb') as decrypted_image_file:
+        decrypted_image_file.write(decrypted_image_data)
+
+# # Example usage
+# eth_address = "0x845Dd1869DB1a3915345e49d8286AA47eE78FDd1"
+# etherscan_api_key = "WST2SSM3GANQSY9538B87M845XSZPHG9Q5"
+# chain = "SEP"
+# image_path = "original_image.png"
+# INFURA_URL_TESTNET = "https://sepolia.infura.io/v3/12c418a774064ad2b3f241c4dd932dee"
+# image_file = "original_image.png"
+
+# # Read the image as binary data
+# with open(image_path, 'rb') as image_file:
+#     image_data_bytes = image_file.read()
+
+# symmetric_key_hash = encrypt_image_with_eth_address(eth_address, etherscan_api_key, chain, image_data_bytes, INFURA_URL_TESTNET)
+# symmetric_key_hash = "BGYnHvNZQAlJbRXfo3I8YDqBonAVs9gNBvYJ3qEeZrc+BFYjTSTghJLYc0A0imETb82U0lqrXs3vGqPoCRxlhqcApsm75VDMllRM5jTjFYjPZkCLOBEVAUyKdZz6iAklNDPVPdCupMgVG5DWGLJcMAvlg8qkenSKUG8uRHG++vm7av71n89Uiv4gZKBs"
+# decrypt_image(os.environ.get('PRIVATE_KEY'), symmetric_key_hash, "encrypted_image.enc")
---
+++ WorldHost/server.py
@@ -0,0 +1,414 @@
+from fastapi import FastAPI, File, UploadFile, HTTPException, Form
+from typing import Optional
+from cryptography.fernet import Fernet
+import base64
+import json
+import os
+from httpx import AsyncClient
+from parse_public import eth_address_to_pub_key
+from ecies import encrypt, decrypt
+from io import BytesIO
+from azure.storage.blob import BlobServiceClient, ContainerClient
+from azure.data.tables import TableServiceClient, TableEntity
+import threading
+from typing import List
+from urllib.parse import urlparse
+import hashlib
+
+lock = threading.Lock()
+
+app = FastAPI()
+
+ITEMS_PER_PAGE = 10  # Define how many items you want per page
+
+# Set your NFT.storage API key in environment variable
+nft_storage_api_key = os.environ.get('NFT_STORAGE_API_KEY')
+# Set Etherscan API Key
+etherscan_api_key = os.environ.get('ETHERSCAN_API_KEY')
+# Set Web3 Provider
+web3provider = os.environ.get('WEB3_PROVIDER')
+
+# Azure storage account details
+account_name = "worlddexstorage"
+account_key = os.environ.get("BLOB_ACCOUNT_KEY")
+connection_string = f"DefaultEndpointsProtocol=https;AccountName={account_name};AccountKey={account_key};EndpointSuffix=core.windows.net"
+
+# Initialize Azure Blob Service Client
+blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+# Initialize Azure Table Service Client
+table_service_client = TableServiceClient.from_connection_string(connection_string)
+table_client = table_service_client.get_table_client(table_name="dextablestorage")
+users_table_client = table_service_client.get_table_client(table_name="users")
+
+@app.post("/registerUser")
+async def register_user(username: str, password: str, ethereum_address: str = None):
+    try:
+        # Check if username already exists
+        filter_query = f"PartitionKey eq '{username}'"
+        existing_users = list(users_table_client.query_entities(filter_query))
+        if existing_users:
+            raise HTTPException(status_code=400, detail="Username already exists")
+
+        # Hash the password
+        hashed_password = hash_password(password)
+
+        # Create user entity
+        user_entity = TableEntity()
+        user_entity['PartitionKey'] = username
+        user_entity['RowKey'] = 'userinfo'  # Or some other suitable row key
+        user_entity['Password'] = hashed_password
+        user_entity['EthereumAddress'] = ethereum_address
+
+        # Add to table
+        users_table_client.create_entity(user_entity)
+
+        return {"message": "User registered successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+      
+@app.post("/login")
+async def login(username: str = Form(...), password: str = Form(...)):
+    try:
+        # Check if user exists
+        filter_query = f"PartitionKey eq '{username}' and RowKey eq 'userinfo'"
+        user_entities = list(users_table_client.query_entities(filter_query))
+
+        if not user_entities:
+            return {"status": "error", "message": "Invalid username or password"}
+
+        user_entity = user_entities[0]
+        hashed_password = hash_password(password)
+
+        # Verify password
+        if user_entity['Password'] == hashed_password:
+            return {"status": "success", "message": "Login successful"}
+        else:
+            return {"status": "error", "message": "Invalid username or password"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+      
+@app.get("/getAllUsernames")
+async def get_all_usernames():
+    try:
+        # Query to retrieve all user entities
+        filter_query = "RowKey eq 'userinfo'"  # Assuming 'userinfo' is the RowKey for all user entries
+        user_entities = users_table_client.query_entities(filter_query)
+
+        # Extracting usernames (PartitionKey)
+        usernames = [entity['PartitionKey'] for entity in user_entities]
+
+        return {"usernames": usernames}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get("/getSpecificImage")
+async def get_image(image_id: str):
+    # try:
+        # Query the table to find the entity with the given row_key
+        filter_query = f"RowKey eq '{image_id}'"
+        entities = list(table_client.query_entities(filter_query))
+
+        if not entities:
+            raise HTTPException(status_code=404, detail="Image not found")
+
+        # Assuming the first matching entity contains the image URL
+        blob_url = entities[0].get('ImageBlobURL')
+
+        # Create a blob client using the blob's URL
+        container_name, blob_name = url_to_blob(blob_url)  # Assuming you have this function defined
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+
+        # Download the blob contents
+        download_stream = blob_client.download_blob()
+        image_bytes = download_stream.readall()
+
+        # Convert bytes to base64 encoded string
+        base64_encoded = base64.b64encode(image_bytes).decode('utf-8')
+
+        return {"image_data": base64_encoded}
+    # except Exception as e:
+    #     raise HTTPException(status_code=500, detail=f"Error retrieving image: {str(e)}")
+
+
+@app.get("/getUserImages")
+async def get_user_images(user_id: str):
+    try:
+      # Query to retrieve images belonging to user_id
+      filter_query = f"PartitionKey eq '{user_id}'"
+      queried_entities = table_client.query_entities(filter_query)
+
+      # Fetching Images
+      result = []
+      for entity in queried_entities:
+          blob_url = entity.get('ImageBlobURL')
+          container_name, blob_name = url_to_blob(blob_url)  # Assuming you have this function defined
+          blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+          download_stream = blob_client.download_blob()
+          image_bytes = download_stream.readall()
+          base64_encoded_image = base64.b64encode(image_bytes).decode('utf-8')
+
+          # Add the base64 encoded image to the result
+          entity_with_image = entity.copy()
+          entity_with_image['Base64Image'] = base64_encoded_image
+          result.append(entity_with_image)
+
+      if not result:
+          return {"message": "No images found for the user"}
+
+      return {"images": result}
+
+    except Exception as e:
+      print(f"Error retrieving data: {e}")
+      raise HTTPException(status_code=500, detail="Internal Server Error")
+
+
+@app.get("/excludeUserImages")
+async def exclude_user_images(user_id: str, page: int = 1):
+    # try:
+      # Calculate the range of items for the requested page
+      start_index = (page - 1) * ITEMS_PER_PAGE
+      end_index = start_index + ITEMS_PER_PAGE
+
+      # Create a TableClient
+      # Query to retrieve images not belonging to user_id
+      filter_query = f"PartitionKey ne '{user_id}'"
+      queried_entities = table_client.query_entities(filter_query)
+
+      # Implementing Pagination and Image Fetching
+      result = []
+      for i, entity in enumerate(queried_entities):
+          if start_index <= i < end_index:
+              # Fetch and encode the image
+              blob_url = entity.get('ImageBlobURL')
+              print(f'blob_url: {blob_url}\n')
+              container_name, blob_name = url_to_blob(blob_url)
+              blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+              download_stream = blob_client.download_blob()
+              image_bytes = download_stream.readall()
+              base64_encoded_image = base64.b64encode(image_bytes).decode('utf-8')
+                
+              # Add the base64 encoded image to the result
+              entity_with_image = entity.copy()
+              entity_with_image['Base64Image'] = base64_encoded_image
+              result.append(entity_with_image)
+          elif i >= end_index:
+              break
+
+      if not result:
+          return {"message": "No more images found"}
+
+      return {"images": result}
+
+    # except Exception as e:
+    #     print(f"Error retrieving data: {e}")
+    #     raise HTTPException(status_code=500, detail="Internal Server Error")
+
+@app.post("/upload/")
+async def upload(
+    image_base64: str = Form(...), 
+    cropped_image_base64: str = Form(...), 
+    decentralize_storage: Optional[str] = Form(False), 
+    eth_address: Optional[str] = Form(None), 
+    user_id: str = Form(...), 
+    location_taken: str = Form(...), 
+    details: str = Form(...), 
+    probability: str = Form(...),
+    image_classification: str = Form(...)
+):
+  
+    if not await is_valid_username(user_id):
+      raise HTTPException(status_code=400, detail="Invalid user ID")
+        
+    decentralize_storage_bool = decentralize_storage.lower() in ["true", "1", "t", "y", "yes"]
+
+    # Convert base64 images back to bytes for decentralized upload
+    cropped_image_content = base64.b64decode(cropped_image_base64)
+    decentralized_upload_successful = False
+    metadata_cid = None
+    metadata_url = ""
+    
+    if decentralize_storage_bool:
+        try:
+          public_key, _ = eth_address_to_pub_key(eth_address, etherscan_api_key, "SEP", web3provider)
+          public_key_hex = public_key.to_hex()
+          encrypted_key, encrypted_image = encrypt_image(cropped_image_content, public_key_hex)
+
+          # NFT Storage Upload
+          async with AsyncClient() as client:
+              image_cid = await upload_to_nft_storage(client, encrypted_image, image_classification)
+
+              # Prepare metadata
+              metadata = {
+                  "name": "Encrypted Image",
+                  "description": "An encrypted image with its encrypted symmetric key",
+                  "image": f"ipfs://{image_cid}",
+                  "properties": {"encrypted_key": encrypted_key}
+              }
+
+              metadata_cid = await upload_metadata_to_nft_storage(client, metadata, image_cid)
+              metadata_url = f"https://{metadata_cid}.ipfs.nftstorage.link"
+              decentralized_upload_successful = True
+
+        except Exception as e:
+          print(f"Decentralized Upload failed: {e}")
+
+     # Centralized upload logic
+    try:
+        image_id = get_next_image_id()  # Get a unique image identifier
+
+        # Call to centralized upload function
+        centralized_upload_response = await centralized_upload(
+            image_base64=image_base64,
+            cropped_image_base64=cropped_image_base64,
+            user_id=user_id,
+            image_id=image_id,
+            location_taken=location_taken,
+            user_address=eth_address,
+            details=details,
+            probability=probability,
+            image_classification=image_classification,
+            ipfs_cid=metadata_cid or "N/A"
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Centralized upload failed: {str(e)}")
+
+    return {
+        "decentralized_upload": decentralized_upload_successful,
+        "centralized_upload": centralized_upload_response,
+        "message": "Upload process completed.",
+        "metadata_cid": metadata_cid,
+        "metadata_url": metadata_url
+    }
+    
+async def upload_to_nft_storage(client, encrypted_image, image_classification):
+    image_response = await client.post(
+        'https://api.nft.storage/upload',
+        files={'file': (image_classification, encrypted_image, 'application/octet-stream')},
+        headers={'Authorization': f'Bearer {nft_storage_api_key}'}
+    )
+    if image_response.status_code == 200:
+        image_cid = image_response.json().get('value', {}).get('cid')
+        return image_cid
+    else:
+        raise HTTPException(status_code=image_response.status_code, detail="Failed to upload to NFT Storage")
+
+async def upload_metadata_to_nft_storage(client, metadata, image_cid):
+    metadata_bytes = json.dumps(metadata).encode('utf-8')
+    metadata_file = BytesIO(metadata_bytes)
+    metadata_file.seek(0)
+    metadata_response = await client.post(
+        'https://api.nft.storage/upload',
+        files={'file': ('metadata.json', metadata_file, 'application/octet-stream')},
+        headers={'Authorization': f'Bearer {nft_storage_api_key}'}
+    )
+    if metadata_response.status_code == 200:
+        metadata_cid = metadata_response.json().get('value', {}).get('cid')
+        return metadata_cid
+    else:
+        raise HTTPException(status_code=metadata_response.status_code, detail="Failed to upload metadata to NFT Storage")
+    
+
+# New endpoint for centralized storage upload
+@app.post("/centralized_upload/")
+async def centralized_upload(image_base64: str = Form(...), cropped_image_base64: str = Form(...), user_id: str = Form(...), image_id: str = Form(...), location_taken: str = Form(...), user_address: str = Form(...), details: str = Form(...), probability: str = Form(...), ipfs_cid: str = Form(...), image_classification: str = Form(...)):
+    try:
+        # Convert base64 image to bytes
+        image_data = base64.b64decode(image_base64)
+        cropped_image_data = base64.b64decode(cropped_image_base64)
+
+        # Upload images to blob storage
+        image_blob_url = upload_image_to_blob("dex-images", f"{image_id}.png", BytesIO(image_data))
+        cropped_image_blob_url = upload_image_to_blob("dex-images", f"{image_id}_cropped.png", BytesIO(cropped_image_data))
+
+        # Create a table entry
+        create_table_entry(user_id, image_id, location_taken, user_address, details, probability, image_blob_url, cropped_image_blob_url, ipfs_cid, image_classification)
+
+        return {"status": "success", "message": "Image and metadata successfully uploaded to centralized storage."}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+  
+# Updated encrypt_image function
+def encrypt_image(image_data_bytes, eth_public_key_hex):
+    symmetric_key = Fernet.generate_key()
+    cipher_suite = Fernet(symmetric_key)
+    encrypted_image = cipher_suite.encrypt(image_data_bytes)
+    encrypted_symmetric_key = encrypt(eth_public_key_hex, symmetric_key)
+    
+    return base64.b64encode(encrypted_symmetric_key).decode(), encrypted_image
+  
+async def upload_to_nft_storage(client, encrypted_image, image_classification):
+    image_response = await client.post(
+        'https://api.nft.storage/upload',
+        files={'file': (image_classification, encrypted_image, 'application/octet-stream')},
+        headers={'Authorization': f'Bearer {nft_storage_api_key}'}
+    )
+    if image_response.status_code == 200:
+        image_cid = image_response.json().get('value', {}).get('cid')
+        return image_cid
+    else:
+        raise HTTPException(status_code=image_response.status_code, detail="Failed to upload to NFT Storage")
+
+def get_next_image_id():
+    lock.acquire()
+    try:
+        # Read the current ID
+        with open("next_id.txt", "r") as file:
+            current_id = int(file.read().strip())
+
+        # Increment and save the next ID
+        with open("next_id.txt", "w") as file:
+            file.write(str(current_id + 1))
+
+        return str(current_id)
+    finally:
+        lock.release()
+    
+# Function to upload an image to blob storage
+def upload_image_to_blob(container_name, blob_name, image_data):
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+    blob_client.upload_blob(image_data)
+    return blob_client.url
+
+# Function to create a table entry
+def create_table_entry(user_id, image_id, location_taken, user_address, details, probability, image_blob_url, cropped_image_blob_url, ipfs_cid, image_classification):
+    entity = TableEntity()
+    entity['PartitionKey'] = user_id
+    entity['RowKey'] = image_id
+    entity['LocationTaken'] = location_taken
+    entity['UserAddress'] = user_address
+    entity['Details'] = details
+    entity['Probability'] = probability
+    entity['ImageBlobURL'] = image_blob_url
+    entity['CroppedImageBlobURL'] = cropped_image_blob_url
+    entity['IPFSCid'] = ipfs_cid
+    entity['ImageClassification'] = image_classification
+    table_client.create_entity(entity)
+    
+def url_to_blob(blob_url):
+  parsed_url = urlparse(blob_url)
+  path_parts = parsed_url.path.lstrip('/').split('/', 1)
+  return path_parts[0], path_parts[1]
+
+def hash_password(password: str) -> str:
+    # Simple hashing for demonstration. Consider a stronger method for production.
+    return hashlib.sha256(password.encode()).hexdigest()
+  
+async def is_valid_username(user_id: str) -> bool:
+    try:
+        # Query to check if the user exists
+        filter_query = f"PartitionKey eq '{user_id}' and RowKey eq 'userinfo'"
+        user_entities = list(users_table_client.query_entities(filter_query))
+
+        return len(user_entities) > 0
+    except Exception as e:
+        print(f"Error checking user validity: {e}")
+        return False  # or raise an exception based on your error handling strategy
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
