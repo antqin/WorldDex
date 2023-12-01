4a6fa4eccd2044beba41c6e4b88c2b3d2c93b82b
Jaiden Reddy <jaidenreddy@gmail.com>
2023-12-01 10:25:43 -0800

    update with server endpoint

--- WorldDex App/WorldDex/Constants.swift
+++ WorldDex App/WorldDex/Constants.swift
@@ -6,7 +6,7 @@
 //
 
 struct Constants {
-    static let baseURL = "https://worlddex.ngrok.app"
+    static let baseURL = "http://52.249.222.215:8000"
     static let inferenceURL = "http://104.42.251.202:5000"
     static let respondURL = ""
     struct Endpoints {
